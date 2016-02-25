770436f3c6ffcfb6cec810beb6f325702d876d92
Jefferson Neel <jefferson.neel@gmail.com>
2016-02-25 17:02:43 -0500

    add functionality to communicate with flickr api

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -7,34 +7,37 @@ class Photo
     FlickRaw.api_key="#{ENV['FLICKR_KEY']}"
     FlickRaw.shared_secret="#{ENV['FLICKR_SECRET']}"
 
-    byebug
-
     flickr = FlickRaw::Flickr.new
 
-    token = flickr.get_request_token
-    auth_url = flickr.get_authorize_url(token['oauth_token'], :perms => 'delete')
-
-    puts "Open this url in your process to complete the authication process : #{auth_url}"
-    puts "Copy here the number given when you complete the process."
-    verify = gets.strip
-
-    begin
-      flickr.get_access_token(token['oauth_token'], token['oauth_token_secret'], verify)
-      login = flickr.test.login
-      puts "You are now authenticated as #{login.username} with token #{flickr.access_token} and secret #{flickr.access_secret}"
-    rescue FlickRaw::FailedResponse => e
-      puts "Authentication failed : #{e.msg}"
-    end
-
-    # list   = flickr.photos.getRecent
+    # token = flickr.get_request_token
+    # auth_url = flickr.get_authorize_url(token['oauth_token'], :perms => 'delete')
     #
-    # id     = list[0].id
-    # secret = list[0].secret
-    # info = flickr.photos.getInfo :photo_id => id, :secret => secret
+    # puts "Open this url in your process to complete the authication process : #{auth_url}"
+    # puts "Copy here the number given when you complete the process."
+    # verify = gets.strip
     #
-    # puts info.title           # => "PICT986"
-    # puts info.dates.taken     # => "2006-07-06 15:16:18"
-    #
+    # begin
+    #   flickr.get_access_token(token['oauth_token'], token['oauth_token_secret'], verify)
+    #   login = flickr.test.login
+    #   puts "You are now authenticated as #{login.username} with token #{flickr.access_token} and secret #{flickr.access_secret}"
+    # rescue FlickRaw::FailedResponse => e
+    #   puts "Authentication failed : #{e.msg}"
+    # end
+
+    list   = flickr.photos.search(:tags => "happydog")
+
+    puts list.length
+
+    random = rand(list.length)
+
+    id     = list[random].id
+    secret = list[random].secret
+    info = flickr.photos.getInfo :photo_id => id, :secret => secret
+
+    puts info.title           # => "PICT986"
+    puts info.dates.taken
+    puts FlickRaw.url_short(info)    # => "2006-07-06 15:16:18"
+
     # sizes = flickr.photos.getSizes :photo_id => id
     #
     # original = sizes.find {|s| s.label == 'Original' }
