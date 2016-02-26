ab4aca569753c6b9fe2f0b965774b366b01caa94
macGH <just_mac@hotmail.com>
2016-02-26 14:21:31 -0500

    Add dog rating

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -9,21 +9,6 @@ class Photo
 
     flickr = FlickRaw::Flickr.new
 
-    # token = flickr.get_request_token
-    # auth_url = flickr.get_authorize_url(token['oauth_token'], :perms => 'delete')
-    #
-    # puts "Open this url in your process to complete the authication process : #{auth_url}"
-    # puts "Copy here the number given when you complete the process."
-    # verify = gets.strip
-    #
-    # begin
-    #   flickr.get_access_token(token['oauth_token'], token['oauth_token_secret'], verify)
-    #   login = flickr.test.login
-    #   puts "You are now authenticated as #{login.username} with token #{flickr.access_token} and secret #{flickr.access_secret}"
-    # rescue FlickRaw::FailedResponse => e
-    #   puts "Authentication failed : #{e.msg}"
-    # end
-
     list   = flickr.people.getPhotos(:user_id => "140515576@N07",
                                      :tags => "seriousdog")
 
@@ -36,27 +21,8 @@ class Photo
 
     info = flickr.photos.getInfo :photo_id => id, :secret => secret
 
-    p "https://farm#{farm}.staticflickr.com/#{server}/#{id}_#{secret}.jpg"
+    picture_link = "https://farm#{farm}.staticflickr.com/#{server}/#{id}_#{secret}.jpg"
 
-    # p FlickRaw.methods
-
-    # puts
-    #
-    # puts info.title           # => "PICT986"
-    # puts info.dates.taken
-    # puts FlickRaw.url_short(info)
-    # puts FlickRaw.to_json(info)    # => "2006-07-06 15:16:18"
-    #
-    # sizes = flickr.photos.getSizes :photo_id => id
-    #
-    # original = sizes.find {|s| s.label == 'Original' }
-    # puts original.width       # => "800" -- may fail if they have no original marked image
-
-
-
-    # @web_string = "https://$#{ENV['SHUTTER_KEY']}:$#{ENV['SHUTTER_SECRET']}@api.shutterstock.com/v2/images/search?query=donkey"
-    # @results = HTTParty.get(@web_string)
-    # p @results
   end
 end
 
